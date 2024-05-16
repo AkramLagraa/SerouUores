00d1d1813c504da0804c97c18804e0f1cd8c02ca
akram <aargalmarka@gmail.com>
2024-05-16 11:26:47 -0400

    first commit X

---
+++ src/components/Text.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Text() {
+  return (
+    <div className="absolute flex flex-col text-[5.5vw] uppercase w-[80vw] items-start leading-tight">
+      <p>Serine</p>
+      <p className="self-end">Is The Cuttest Pretiest</p>
+      <p>Lovely Girl</p>
+      <p className="self-end">Ever</p>
+    </div>
+  )
+}
